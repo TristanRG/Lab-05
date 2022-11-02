9b00582a7d54d00b694eabcdd1bb872ff670586c
TristanRG <115103112+TristanRG@users.noreply.github.com>
2022-11-02 14:36:40 +0200

    Update MyCharacter.java

--- src/MyCharacter.java
+++ src/MyCharacter.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class MyCharacter {
+public class MyCharacter {
+    private static String culoare;
+
+    private static String nume;
+    private static int numarOchi;
+    private static int numarVieti;
+
+    public static String getCuloare() {
+        return culoare;
+    }
+
+    public void setCuloare(String culoare) {
+        this.culoare = culoare;
+    }
+    public static String getNume() {
+        return nume;
+    }
+
+    public void setNume(String nume) {
+        this.nume = nume;
+    }
+
+    public static int getNumarOchi() {
+        return numarOchi;
+    }
+
+    public void setNumarOchi(int numarOchi) {
+        this.numarOchi = numarOchi;
+    }
+
+    public static int getNumarVieti() {
+        return numarVieti;
+    }
+
+    public void setNumarVieti(int numarVieti) {
+        this.numarVieti = numarVieti;
+    }
 }
+
